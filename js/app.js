4d82755ff534b4b90b2c1130b768b8220b087a89
Aurelia Moser <auremoser@gmail.com>
2013-10-08 16:19:28 -0400

    update map.js bc mapbox not providing baselayers *bummer*
    lines 23-24 (thanks andrew for the tip! *waves*)

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@
       , map = new L.Map(mapCanvas).setView(new L.LatLng(40.73402305751195, -74.00871276855469), defaultZoom)
         // If you don't want Stamen Tiles, uncomment these next two lines and comment out the Stamen line
         // there's also a Stamen.js that you can comment out too
-      , mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/cartodb.map-1nh578vv/{z}/{x}/{y}.png'
-      , layer = new L.TileLayer(mapboxUrl, {maxZoom: 18, attribution: "Powered by Leaflet and Mapbox"});
+      , stamenUrl = 'http://tile.stamen.com/toner/{z}/{x}/{y}.png'
+      , layer = new L.TileLayer(stamenUrl, {maxZoom: 18, attribution: "Powered by Leaflet and Stamen"});
       //, layer = new L.StamenTileLayer("watercolor");
 
     map.addLayer(layer,true);
